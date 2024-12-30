824003a648bd1f871e215d6aa4d104c4070540ab
pratama6624 <scyoung6624@gmail.com>
2024-12-30 23:38:21 +0700

    Adding project RickAndMorty in BaseOnProject

---
+++ BaseOnProject/RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Pratama One on 30/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
